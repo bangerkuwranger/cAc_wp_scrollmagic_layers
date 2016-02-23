0fa025557a68d570c0601f1fa11e85b34b17f1e8
Chad A. Carino <chadc@tekserve.com>
2016-02-22 21:30:00 -0500

    tired enough, infact, to fail inconsistently.

--- js/layers.js
+++ js/layers.js
@@ -13,23 +13,23 @@ jQuery(function($) {
 		$media = $(this).find('.cAc_wpsml-media'),
 		$trim = $(this).find('.cAc_wpsml-trim');
 		
-		tweens[id] = new TimelineMax().add([
+		tweens[thisId] = new TimelineMax().add([
 	
-			TweenMax.to("#section-" + id + ".cAc_wpsml-bg", 1, {top: "-40%", ease: Linear.easeNone}),
-			TweenMax.to("#section-" + id + ".cAc_wpsml-mg", 1, {top: "-500%", ease: Linear.easeNone}),
-			TweenMax.to("#section-" + id + ".cAc_wpsml-content", 1, {top: "-225%", ease: Linear.easeNone}),
-			TweenMax.to("#section-" + id + ".cAc_wpsml-media", 1, {top: "-600%", ease: Linear.easeNone}),
-			TweenMax.to("#section-" + id + ".cAc_wpsml-trim", 1, {top: "-800%", ease: Linear.easeNone})
+			TweenMax.to("#section-" + thisId + ".cAc_wpsml-bg", 1, {top: "-40%", ease: Linear.easeNone}),
+			TweenMax.to("#section-" + thisId + ".cAc_wpsml-mg", 1, {top: "-500%", ease: Linear.easeNone}),
+			TweenMax.to("#section-" + thisId + ".cAc_wpsml-content", 1, {top: "-225%", ease: Linear.easeNone}),
+			TweenMax.to("#section-" + thisId + ".cAc_wpsml-media", 1, {top: "-600%", ease: Linear.easeNone}),
+			TweenMax.to("#section-" + thisId + ".cAc_wpsml-trim", 1, {top: "-800%", ease: Linear.easeNone})
 	
 		]);	//end new TimelineMax ()
 		
-		scene[id] = new ScrollScene({triggerElement: "#" + thisId, duration: 2000, offset: 450})
-						.setTween(tweens[id])
+		scene[thisId] = new ScrollScene({triggerElement: "#" + thisId, duration: 2000, offset: 450})
+						.setTween(tweens[thisId])
 						.setPin("#" + thisId)
 						.addTo(controller);
 
 		// show indicators (requires debug extension)
-		scene[id].addIndicators();
+		scene[thisId].addIndicators();
 		
 	
 	});	//end $('.cAc_wpsml-pageSection').each( function()
