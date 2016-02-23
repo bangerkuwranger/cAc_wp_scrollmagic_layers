af2643642c4cf0afd9ae08d8dd778463159bfc52
Chad A. Carino <chadc@tekserve.com>
2016-02-22 20:32:04 -0500

    Start that shortcode.

--- cAc_wp_scrollmagic_layers.php
+++ cAc_wp_scrollmagic_layers.php
@@ -38,60 +38,115 @@ function cAc_wpsml_frontend_queue() {
 }	//end cAc_wpsml_frontend_queue()
 
 
-if ( ! function_exists('cAc_wpsml_section') ) {
+if( ! function_exists('cAc_wpsml_section') ) {
 
-// Register Custom Post Type
-function cAc_wpsml_section() {
+	// Register Custom Post Type
+	function cAc_wpsml_section() {
 
-	$labels = array(
-		'name'                  => _x( 'Layered Page Sections', 'Post Type General Name', 'cAc_wpsml' ),
-		'singular_name'         => _x( 'Layered Page Section', 'Post Type Singular Name', 'cAc_wpsml' ),
-		'menu_name'             => __( 'Layered Page Sections', 'cAc_wpsml' ),
-		'name_admin_bar'        => __( 'Layered Page Sections', 'cAc_wpsml' ),
-		'archives'              => __( 'Layered Page Section Archives', 'cAc_wpsml' ),
-		'parent_item_colon'     => __( 'Parent Section:', 'cAc_wpsml' ),
-		'all_items'             => __( 'All Layered Page Sections', 'cAc_wpsml' ),
-		'add_new_item'          => __( 'Add New Layered Page Section', 'cAc_wpsml' ),
-		'add_new'               => __( 'Add New', 'cAc_wpsml' ),
-		'new_item'              => __( 'New Layered Page Section', 'cAc_wpsml' ),
-		'edit_item'             => __( 'Edit Layered Page Section', 'cAc_wpsml' ),
-		'update_item'           => __( 'Update Layered Page Section', 'cAc_wpsml' ),
-		'view_item'             => __( 'View Layered Page Section', 'cAc_wpsml' ),
-		'search_items'          => __( 'Search Layered Page Sections', 'cAc_wpsml' ),
-		'not_found'             => __( 'Not found', 'cAc_wpsml' ),
-		'not_found_in_trash'    => __( 'Not found in Trash', 'cAc_wpsml' ),
-		'featured_image'        => __( 'Featured Image', 'cAc_wpsml' ),
-		'set_featured_image'    => __( 'Set featured image', 'cAc_wpsml' ),
-		'remove_featured_image' => __( 'Remove featured image', 'cAc_wpsml' ),
-		'use_featured_image'    => __( 'Use as featured image', 'cAc_wpsml' ),
-		'insert_into_item'      => __( 'Insert into Layered Page Section', 'cAc_wpsml' ),
-		'uploaded_to_this_item' => __( 'Uploaded to this Layered Page Section', 'cAc_wpsml' ),
-		'items_list'            => __( 'Layered Page Section list', 'cAc_wpsml' ),
-		'items_list_navigation' => __( 'Layered Page Section list navigation', 'cAc_wpsml' ),
-		'filter_items_list'     => __( 'Filter Layered Page Section list', 'cAc_wpsml' ),
+		$labels = array(
+			'name'                  => _x( 'Layered Page Sections', 'Post Type General Name', 'cAc_wpsml' ),
+			'singular_name'         => _x( 'Layered Page Section', 'Post Type Singular Name', 'cAc_wpsml' ),
+			'menu_name'             => __( 'Layered Page Sections', 'cAc_wpsml' ),
+			'name_admin_bar'        => __( 'Layered Page Sections', 'cAc_wpsml' ),
+			'archives'              => __( 'Layered Page Section Archives', 'cAc_wpsml' ),
+			'parent_item_colon'     => __( 'Parent Section:', 'cAc_wpsml' ),
+			'all_items'             => __( 'All Layered Page Sections', 'cAc_wpsml' ),
+			'add_new_item'          => __( 'Add New Layered Page Section', 'cAc_wpsml' ),
+			'add_new'               => __( 'Add New', 'cAc_wpsml' ),
+			'new_item'              => __( 'New Layered Page Section', 'cAc_wpsml' ),
+			'edit_item'             => __( 'Edit Layered Page Section', 'cAc_wpsml' ),
+			'update_item'           => __( 'Update Layered Page Section', 'cAc_wpsml' ),
+			'view_item'             => __( 'View Layered Page Section', 'cAc_wpsml' ),
+			'search_items'          => __( 'Search Layered Page Sections', 'cAc_wpsml' ),
+			'not_found'             => __( 'Not found', 'cAc_wpsml' ),
+			'not_found_in_trash'    => __( 'Not found in Trash', 'cAc_wpsml' ),
+			'featured_image'        => __( 'Featured Image', 'cAc_wpsml' ),
+			'set_featured_image'    => __( 'Set featured image', 'cAc_wpsml' ),
+			'remove_featured_image' => __( 'Remove featured image', 'cAc_wpsml' ),
+			'use_featured_image'    => __( 'Use as featured image', 'cAc_wpsml' ),
+			'insert_into_item'      => __( 'Insert into Layered Page Section', 'cAc_wpsml' ),
+			'uploaded_to_this_item' => __( 'Uploaded to this Layered Page Section', 'cAc_wpsml' ),
+			'items_list'            => __( 'Layered Page Section list', 'cAc_wpsml' ),
+			'items_list_navigation' => __( 'Layered Page Section list navigation', 'cAc_wpsml' ),
+			'filter_items_list'     => __( 'Filter Layered Page Section list', 'cAc_wpsml' ),
+		);
+		$args = array(
+			'label'                 => __( 'Layered Page Section', 'cAc_wpsml' ),
+			'description'           => __( 'A layered section to be added to a page via shortcode', 'cAc_wpsml' ),
+			'labels'                => $labels,
+			'supports'              => array( 'title', 'editor', 'thumbnail', ),
+			'hierarchical'          => false,
+			'public'                => true,
+			'show_ui'               => true,
+			'show_in_menu'          => true,
+			'menu_position'         => 20,
+			'menu_icon'             => 'dashicons-admin-page',
+			'show_in_admin_bar'     => true,
+			'show_in_nav_menus'     => true,
+			'can_export'            => true,
+			'has_archive'           => false,		
+			'exclude_from_search'   => true,
+			'publicly_queryable'    => true,
+			'capability_type'       => 'page',
+		);
+		register_post_type( 'cAcsmlsection', $args );
+
+	}	//end cAc_wpsml_section()
+	add_action( 'init', 'cAc_wpsml_section', 0 );
+
+}	//end if( ! function_exists('cAc_wpsml_section') )
+
+
+
+// Add Shortcode
+function cAc_wpsml_section_shortcode( $atts ) {
+
+	// Attributes
+	extract( shortcode_atts(
+		array(
+			'id' => 'null',
+		), $atts )
 	);
-	$args = array(
-		'label'                 => __( 'Layered Page Section', 'cAc_wpsml' ),
-		'description'           => __( 'A layered section to be added to a page via shortcode', 'cAc_wpsml' ),
-		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'thumbnail', ),
-		'hierarchical'          => false,
-		'public'                => true,
-		'show_ui'               => true,
-		'show_in_menu'          => true,
-		'menu_position'         => 20,
-		'menu_icon'             => 'dashicons-admin-page',
-		'show_in_admin_bar'     => true,
-		'show_in_nav_menus'     => true,
-		'can_export'            => true,
-		'has_archive'           => false,		
-		'exclude_from_search'   => true,
-		'publicly_queryable'    => true,
-		'capability_type'       => 'page',
-	);
-	register_post_type( 'cAcsmlsection', $args );
 
-}
-add_action( 'init', 'cAc_wpsml_section', 0 );
+	// Code
+if( $id == null ) {
 
-}
+	return '';
+
+}	//end if( $id == null )
+
+$section = get_post( $id );
+
+if( !$section ) {
+
+	return '';
+
+}	//end if( !$section )
+
+$html = '<div class="cAc_wpsml-pageSection">';
+
+$html .= '<div class="cAc_wpsml-bg">';
+
+$html .= '</div>';
+
+$html .= '<div class="cAc_wpsml-mg">';
+
+$html .= '</div>';
+
+$html .= '<div class="cAc_wpsml-content">';
+$html .= apply_filters( 'the_content', $section->post_content  );
+$html .= '</div>';
+
+$html .= '<div class="cAc_wpsml-media">';
+$html .= get_the_post_thumbnail( $section );
+$html .= '</div>';
+
+$html .= '<div class="cAc_wpsml-trim">';
+
+$html .= '</div>';
+
+$html .= '</div>';
+return $html;
+}	//end cAc_wpsml_section_shortcode( $atts )
+add_shortcode( 'smpagesection', 'cAc_wpsml_section_shortcode' );
+
