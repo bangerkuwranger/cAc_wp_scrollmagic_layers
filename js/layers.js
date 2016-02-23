aca81d44c8b22c62f6c7c86b8c35916bfa8b51f0
Chad A. Carino <chadc@tekserve.com>
2016-02-22 21:45:09 -0500

    finally, the triggers are there!

--- js/layers.js
+++ js/layers.js
@@ -4,7 +4,7 @@ scenes = {};
 
 jQuery(function($) {
 	
-	controller = new ScrollMagic();
+	controller = new ScrollMagic.Controller();
 	$('.cAc_wpsml-pageSection').each( function() {
 	
 		var thisId = $(this).attr('id'),
@@ -17,10 +17,10 @@ jQuery(function($) {
 		tweens[thisId] = new TimelineMax().add([
 	
 			TweenMax.to("#section-" + thisId + ".cAc_wpsml-bg", 1, {top: "-40%", ease: Linear.easeNone}),
-			TweenMax.to("#section-" + thisId + ".cAc_wpsml-mg", 1, {top: "-500%", ease: Linear.easeNone}),
-			TweenMax.to("#section-" + thisId + ".cAc_wpsml-content", 1, {top: "-225%", ease: Linear.easeNone}),
-			TweenMax.to("#section-" + thisId + ".cAc_wpsml-media", 1, {top: "-600%", ease: Linear.easeNone}),
-			TweenMax.to("#section-" + thisId + ".cAc_wpsml-trim", 1, {top: "-800%", ease: Linear.easeNone})
+			TweenMax.to("#section-" + thisId + ".cAc_wpsml-mg", 1, {top: "-50%", ease: Linear.easeNone}),
+			TweenMax.to("#section-" + thisId + ".cAc_wpsml-content", 1, {top: "-25%", ease: Linear.easeNone}),
+			TweenMax.to("#section-" + thisId + ".cAc_wpsml-media", 1, {top: "-60%", ease: Linear.easeNone}),
+			TweenMax.to("#section-" + thisId + ".cAc_wpsml-trim", 1, {top: "-80%", ease: Linear.easeNone})
 	
 		]);	//end new TimelineMax ()
 		
